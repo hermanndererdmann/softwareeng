3310fa41127a4707612ea30232c21a1febedff93
Luca Bargetzi <luca.bargetzi@gmail.com>
2023-10-30 15:15:14 +0100

    change color and shape detection

--- src/image processor.py
+++ src/image processor.py
@@ -1,5 +1,5 @@
-import cv2 
-import numpy as np 
+import cv2
+import numpy as np
 
 class ImgData:
     def __init__(self):
@@ -7,60 +7,62 @@ class ImgData:
         self.pattern = []
         self.color = []
         self.contour = []
-
-
+        self.cx = []
+        self.cy = []
 
 class image_processor:
     def __init__(self):
-        self.imgdata=ImgData()
- 
+        self.imgdata = ImgData()
+
     def detect_shapes(self, img):
-
-        # Create lists to store the detected patterns with their shapes and contours
-        detected_patterns = []
-        pattern_shapes = []
-        pattern_contours = []
-
         # setting threshold of gray image 
-        self.gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        self.__gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
         # Apply Gaussian blur to reduce noise
-        self.blurred = cv2.GaussianBlur(self.gray, (5, 5), 0)
+        self.__blurred = cv2.GaussianBlur(self.__gray, (5, 5), 0)
 
         # Detect edges using Canny edge detection
-        self.edges = cv2.Canny(self.blurred, 50, 150)
+        self.__edges = cv2.Canny(self.__blurred, 50, 150)
 
         # Find contours in the edge image
-        contours, _ = cv2.findContours(self.edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        contours, _ = cv2.findContours(self.__edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-        for contour in contours[1:]:
+        for contour in contours:
             # Approximate the shape
             epsilon = 0.04 * cv2.arcLength(contour, True)
             approx = cv2.approxPolyDP(contour, epsilon, True)
 
             # Determine the shape based on the number of vertices
             num_vertices = len(approx)
+            if num_vertices == 3:
+                self.imgdata.pattern.append("Triangle")
+            elif num_vertices == 4:
+                # Check the aspect ratio to determine Rectangle or Square
+                x, y, w, h = cv2.boundingRect(contour)
+                aspect_ratio = float(w) / h
+                if 0.95 <= aspect_ratio <= 1.05:
+                    self.imgdata.pattern.append("Square")
+                else:
+                    self.imgdata.pattern.append("Rectangle")
+            else:
+                self.imgdata.pattern.append("Circle")
 
-            # Get the center of the shape
+            # Calculate the centroid of the contour
             M = cv2.moments(contour)
-            if M['m00'] != 0:
-                cx = int(M['m10'] / M['m00'])
-                cy = int(M['m01'] / M['m00'])
+            cx = int(M['m10'] / M['m00'])
+            cy = int(M['m01'] / M['m00'])
 
-                # Store the detected pattern information
-                detected_patterns.append((cx, cy))
-                pattern_shapes.append(num_vertices)
-                pattern_contours.append(contour)
+            # Store the detected contour information
+            self.imgdata.contour.append(contour)
+            self.imgdata.cx.append(cx)
+            self.imgdata.cy.append(cy)
 
-        return detected_patterns, pattern_shapes, pattern_contours
+            # Call detect_colors to detect color around the centroid
+            self.detect_colors(img, cx, cy)
 
-    def detect_colors(self):
-
-        # Create lists to store the detected colors
-        pattern_colors = []
-
+    def detect_colors(self, img, cx, cy):
         # Convert the image to the HSV color space
-        self.hsv_img = cv2.cvtColor(self.img, cv2.COLOR_BGR2HSV)
+        self.__hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
         # Define color ranges for the specified colors
         color_ranges = {
@@ -71,33 +73,33 @@ class image_processor:
             'violet': [(130, 100, 100), (160, 255, 255)],
         }
 
-        for color, (lower, upper) in color_ranges.items():
-            # Create a mask for the specific color
-            lower_color = np.array(lower, dtype=np.uint8)
-            upper_color = np.array(upper, dtype=np.uint8)
-            color_mask = cv2.inRange(self.hsv_img, lower_color, upper_color)
+        # Extract color from a 5x5 region around the centroid (cx, cy)
+        color_region = self.__hsv_img[cy - 5:cy + 5, cx - 5:cx + 5]
 
-            # Find contours for the color regions
-            contours, _ = cv2.findContours(color_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        # Calculate the average HSV color in the region
+        average_hsv_color = np.mean(color_region, axis=(0, 1)).astype(int)
+        print(average_hsv_color)
 
-            for contour in contours:
-                pattern_colors.append(color)
+        # Classify the color based on the color_ranges
+        color = self.classify_color(average_hsv_color)
 
-        return pattern_colors
+        # Append the detected color's name to the list
+        self.imgdata.color.append(color)
 
-    def process_image(self):
-        
-        # Detect shapes
-        detected_patterns, pattern_shapes, pattern_contours = self.detect_shapes()
-        
-        # Detect colors
-        pattern_colors = self.detect_colors()
+    def classify_color(self, hsv_color):
+        # You can implement a color classification logic here based on the HSV color values.
+        # For now, I'm assuming a simple classification based on hue value.
+        hue = hsv_color[0]
 
-        # Set the processed image data to the imgdata object
-        self.contour = pattern_contours
-        self.imgdata.img = self.img
-        self.imgdata.pattern = pattern_shapes
-        self.imgdata.color = pattern_colors
-        self.imgdata.contour = self.contour
-
-        
+        if 0 <= hue < 10:
+            return 'red'
+        elif 35 <= hue < 85:
+            return 'green'
+        elif 100 <= hue < 130:
+            return 'blue'
+        elif 20 <= hue < 30:
+            return 'yellow'
+        elif 130 <= hue < 160:
+            return 'violet'
+        else:
+            return 'unknown'
