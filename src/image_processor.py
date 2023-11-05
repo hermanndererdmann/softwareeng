5f01ea6a828b11eeb2c9843e1f0711cc87ec70a6
florian <hermanndererdmann@gmail.com>
2023-11-05 11:54:53 +0100

    fixes to image_processor

--- src/image_processor.py
+++ src/image_processor.py
@@ -10,24 +10,38 @@ class ImgData:
         self.cx = []
         self.cy = []
 
-class image_processor:
+class processor:
     def __init__(self):
         self.imgdata = ImgData()
 
+
     def detect_shapes(self, img):
+        
+        pattern = []
+        self.imgdata.color = []
+        cx = []
+        cy = []
+        cv2.imshow('Camera Feed', img)
         # setting threshold of gray image 
-        self.__gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
         # Apply Gaussian blur to reduce noise
-        self.__blurred = cv2.GaussianBlur(self.__gray, (5, 5), 0)
+        blurred = cv2.GaussianBlur(gray, (5, 5), 0)
 
         # Detect edges using Canny edge detection
-        self.__edges = cv2.Canny(self.__blurred, 50, 150)
+        edges = cv2.Canny(blurred, 50, 150)
 
         # Find contours in the edge image
-        contours, _ = cv2.findContours(self.__edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
         for contour in contours:
+
+            # filter for the right size
+            if cv2.contourArea(contour) < 40 * 40:
+               continue
+            elif cv2.contourArea(contour) > 400 * 400:
+                continue
+
             # Approximate the shape
             epsilon = 0.04 * cv2.arcLength(contour, True)
             approx = cv2.approxPolyDP(contour, epsilon, True)
@@ -35,71 +49,73 @@ class image_processor:
             # Determine the shape based on the number of vertices
             num_vertices = len(approx)
             if num_vertices == 3:
-                self.imgdata.pattern.append("Triangle")
+                pattern.append("Triangle")
             elif num_vertices == 4:
                 # Check the aspect ratio to determine Rectangle or Square
                 x, y, w, h = cv2.boundingRect(contour)
                 aspect_ratio = float(w) / h
                 if 0.95 <= aspect_ratio <= 1.05:
-                    self.imgdata.pattern.append("Square")
+                    pattern.append("Square")
                 else:
-                    self.imgdata.pattern.append("Rectangle")
+                    pattern.append("Rectangle")
+            elif 5 <= num_vertices <= 8:
+                pattern.append("Circle")
             else:
-                self.imgdata.pattern.append("Circle")
+                pattern.append("Unknown")
 
             # Calculate the centroid of the contour
             M = cv2.moments(contour)
-            cx = int(M['m10'] / M['m00'])
-            cy = int(M['m01'] / M['m00'])
-
+            if M['m00'] != 0.0: 
+                __cx = int(M['m10'] / M['m00'])
+                __cy = int(M['m01'] / M['m00'])
             # Store the detected contour information
-            self.imgdata.contour.append(contour)
-            self.imgdata.cx.append(cx)
-            self.imgdata.cy.append(cy)
+                cx.append(__cx)
+                cy.append(__cy)
+          
+
 
             # Call detect_colors to detect color around the centroid
-            self.detect_colors(img, cx, cy)
+                self.__detect_colors(img, __cx, __cy)
+        self.imgdata.cx = cx
+        self.imgdata.cy = cy
+        self.imgdata.pattern = pattern
 
-    def detect_colors(self, img, cx, cy):
+        print(self.imgdata.color)
+
+
+    def __detect_colors(self, img, cx, cy):
         # Convert the image to the HSV color space
-        self.__hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-
-        # Define color ranges for the specified colors
-        color_ranges = {
-            'red': [(0, 100, 100), (10, 255, 255)],
-            'green': [(35, 100, 100), (85, 255, 255)],
-            'blue': [(100, 100, 100), (130, 255, 255)],
-            'yellow': [(20, 100, 100), (30, 255, 255)],
-            'violet': [(130, 100, 100), (160, 255, 255)],
-        }
+        hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
         # Extract color from a 5x5 region around the centroid (cx, cy)
-        color_region = self.__hsv_img[cy - 5:cy + 5, cx - 5:cx + 5]
+        color_region = hsv_img[cy - 5:cy + 5, cx - 5:cx + 5]
 
         # Calculate the average HSV color in the region
         average_hsv_color = np.mean(color_region, axis=(0, 1)).astype(int)
-        print(average_hsv_color)
 
         # Classify the color based on the color_ranges
-        color = self.classify_color(average_hsv_color)
+        color = self.__classify_color(average_hsv_color)
 
         # Append the detected color's name to the list
         self.imgdata.color.append(color)
 
-    def classify_color(self, hsv_color):
-        # You can implement a color classification logic here based on the HSV color values.
-        # For now, I'm assuming a simple classification based on hue value.
+    def __classify_color(self, hsv_color):
+        
         hue = hsv_color[0]
 
         if 0 <= hue < 10:
             return 'red'
-        elif 35 <= hue < 85:
-            return 'green'
-        elif 100 <= hue < 130:
-            return 'blue'
-        elif 20 <= hue < 30:
+        elif 10 <= hue < 30:
+            return 'orange'
+        elif 30 <= hue < 85:
             return 'yellow'
-        elif 130 <= hue < 160:
-            return 'violet'
+        elif 85 <= hue < 170:
+            return 'green'
+        elif 170 <= hue < 260:
+            return 'blue'
+        elif 260 <= hue < 320:
+            return 'purple'
+        elif 320 <= hue < 360:
+            return 'red'  # Red wraps around, so consider it as a continuation of the red range.
         else:
             return 'unknown'
