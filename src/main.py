90263737fe9171a7793484b9bad717b0c2b8c200
florian <hermanndererdmann@gmail.com>
2023-11-05 11:55:57 +0100

    implement image_processor to main

--- src/main.py
+++ src/main.py
@@ -2,13 +2,26 @@ import cv2
 import numpy as np
 
 from webcam import Camera  # Assuming your camera class is in a 'camera.py' file
+from image_processor import processor
 
 def main():
+    camera_id = 1  # You can change this to the appropriate camera ID (0, 1, 2, etc.) as needed
+    cam = Camera(camera_id)
+    imgprocessor = processor()
+
+
     while True:
-        camera_id = 0  # You can change this to the appropriate camera ID (0, 1, 2, etc.) as needed
-        cam = Camera(camera_id)
         img = cam.get_frame()
         cv2.imshow('Camera Feed', img) #frame
+        imgprocessor.detect_shapes(img)
+
+
+        key = cv2.waitKey(1)
+        if key == 27:
+            break
+
+    cam.release()
+    #cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
