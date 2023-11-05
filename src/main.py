44490c9f73b54a9084a514d662e36067e9021303
florian <hermanndererdmann@gmail.com>
2023-11-05 13:55:32 +0100

    implement visualizer into main

--- src/main.py
+++ src/main.py
@@ -3,25 +3,28 @@ import numpy as np
 
 from webcam import Camera  # Assuming your camera class is in a 'camera.py' file
 from image_processor import processor
+from visualizer import visualizer
 
 def main():
     camera_id = 1  # You can change this to the appropriate camera ID (0, 1, 2, etc.) as needed
     cam = Camera(camera_id)
     imgprocessor = processor()
-
+    overlay_visualizer= visualizer()
+    
 
     while True:
         img = cam.get_frame()
         cv2.imshow('Camera Feed', img) #frame
         imgprocessor.detect_shapes(img)
 
-
+        overlay_visualizer.get_imgdata(imgprocessor.imgdata)
+        overlay_visualizer.show_overlay()
         key = cv2.waitKey(1)
         if key == 27:
             break
 
     cam.release()
-    #cv2.destroyAllWindows()
+    cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
